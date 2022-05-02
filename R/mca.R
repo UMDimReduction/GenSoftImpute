50d16a230d0d88b0fbe1fb946f5ecc72a4f389d6
Max Turgeon <turgeon.maxime@gmail.com>
2022-05-02 16:27:08 -0600

    fix doc warnings

--- R/mca.R
+++ R/mca.R
@@ -1,7 +1,7 @@
 #' Multiple Correspondence Analysis
 #'
 #' The function takes in a data matrix and performs Multiple Correspondence
-#' Analysis (MCP) using Generalized Singular Value Decomposition.
+#' Analysis (MCA) using Generalized Singular Value Decomposition.
 #'
 #' @param X a data matrix to decompose
 #' @param k total number of components to return. If \code{NULL}, the full set
@@ -17,7 +17,7 @@
 #'   \item{v}{Right (columns) generalized singular vectors. Dimensions are
 #'   \code{ncol(X)} by k.}
 #' @export
-MCP <- function(X, k = NULL, tol = .Machine$double.eps^0.5) {
+MCA <- function(X, k = NULL, tol = .Machine$double.eps^0.5) {
   # Prepare data
   observed_matrix <- X/sum(X)
   row_frequencies <- rowSums(observed_matrix)
@@ -47,31 +47,25 @@ MCP <- function(X, k = NULL, tol = .Machine$double.eps^0.5) {
 
 #----
 #' @export
-MCP2 <- function(X, k = NULL, tol = .Machine$double.eps^0.5) {
-  # Prepare data
+#' @rdname MCA
+MCA2 <- function(X, k = NULL, tol = .Machine$double.eps^0.5) {
+  # # Prepare data
   # observed_matrix <- X/sum(X)
   # row_frequencies <- rowSums(observed_matrix)
   # col_frequencies <- colSums(observed_matrix)
   # expected_matrix <- row_frequencies %o% col_frequencies
   # deviations_matrix <- observed_matrix - expected_matrix
-  # # Generalized SVD
-  # Amat <- deviations_matrix
-  # Mmat <- diag(1/row_frequencies)
-  # Wmat <- diag(1/col_frequencies)
   #
-  # Mmat_sqrt <- diag(1/sqrt(row_frequencies))
-  # Wmat_sqrt <- diag(1/sqrt(col_frequencies))
-  #
-  # Atilde <- Mmat_sqrt %*% Amat %*% Wmat_sqrt
-  #
-  # Atilde_svd <- svd(Atilde)
-  # keep <- if (is.null(k)) which(Atilde_svd$d > tol) else k
-  #
-  # Umat <- solve(Mmat_sqrt) %*% Atilde_svd$u[,keep]
-  # Vmat <- solve(Wmat_sqrt) %*% Atilde_svd$v[,keep]
-  #
-  # return(list(d = Atilde_svd$d[keep],
-  #             u = Umat,
-  #             v = Vmat))
-  gen_svd(X, diag(nrow(X)), diag(ncol(X)))
+  # out <- gen_svd(deviations_matrix,
+  #                diag(1/row_frequencies),
+  #                diag(1/col_frequencies))
+
+  out <- gen_svd(X, diag(nrow(X)), diag(ncol(X)))
+
+  keep <- if (is.null(k)) which(drop(out$svalues) > tol) else k
+  out$svalues <- drop(out$svalues[keep,])
+  out$left_svectors <- out$left_svectors[, keep, drop = FALSE]
+  out$right_svectors <- out$right_svectors[, keep, drop = FALSE]
+
+  return(out)
 }
--- src/gen_svd.cpp
+++ src/gen_svd.cpp
@@ -6,16 +6,16 @@ Rcpp::List gen_svd(const Rcpp::NumericMatrix &X,
                    const Rcpp::NumericMatrix &M,
                    const Rcpp::NumericMatrix &W) {
   // Convert to arma::mat
-  arma::mat Xmat = Rcpp::as<arma::mat>(X);
-  arma::mat Mmat = Rcpp::as<arma::mat>(M);
-  arma::mat Wmat = Rcpp::as<arma::mat>(W);
+  const arma::mat Xmat = Rcpp::as<arma::mat>(X);
+  const arma::mat Mmat = Rcpp::as<arma::mat>(M);
+  const arma::mat Wmat = Rcpp::as<arma::mat>(W);
 
   // Compute Cholesky decompositions
-  arma::mat Mmat_sqrt = arma::chol(Mmat);
-  arma::mat Wmat_sqrt = arma::chol(Wmat);
+  const arma::mat Mmat_sqrt = arma::chol(Mmat);
+  const arma::mat Wmat_sqrt = arma::chol(Wmat);
 
   // Generalized SVD
-  arma::mat Xtilde = Mmat_sqrt * Xmat * Wmat_sqrt;
+  const arma::mat Xtilde = Mmat_sqrt * Xmat * Wmat_sqrt;
 
   arma::mat U;
   arma::vec s;
@@ -23,11 +23,7 @@ Rcpp::List gen_svd(const Rcpp::NumericMatrix &X,
 
   arma::svd(U, s, V, Xtilde);
 
-  arma::mat Umat = inv(Mmat_sqrt) * U;
-  arma::mat Vmat = inv(Wmat_sqrt) * V;
-
-  arma::vec d = s.as_col();
-  return Rcpp::List::create(Rcpp::Named("svalues") = d,
-                            Rcpp::Named("left_svectors") = Umat,
-                            Rcpp::Named("right_svectors")  = Vmat);
+  return Rcpp::List::create(Rcpp::Named("svalues") = s.as_col(),
+                            Rcpp::Named("left_svectors") = inv(Mmat_sqrt) * U,
+                            Rcpp::Named("right_svectors")  = inv(Wmat_sqrt) * V);
 }
